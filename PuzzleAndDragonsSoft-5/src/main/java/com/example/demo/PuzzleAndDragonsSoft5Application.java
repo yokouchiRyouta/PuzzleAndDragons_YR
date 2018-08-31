19e324fcb8901d9483c2010ac6e9ecdf8d6c4268
yokouchiRyouta <ryoutako0901@gmail.com>
2018-08-31 17:17:37 +0900

    this is first commit

---
+++ PuzzleAndDragonsSoft-5/src/main/java/com/example/demo/PuzzleAndDragonsSoft5Application.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuzzleAndDragonsSoft5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuzzleAndDragonsSoft5Application.class, args);
+	}
+}
